e03012044a8105e626d343629ff2e214ed2201f8
bouzuya <m@bouzuya.net>
2016-08-15 08:57:12 +0900

    use class in atom-builder

--- src/atom-builder.ts
+++ src/atom-builder.ts
@@ -1,15 +1,13 @@
 import * as moment from 'moment';
 
-var AtomBuilder;
-
-AtomBuilder = (function () {
-  function AtomBuilder(posts) {
+export class AtomBuilder {
+  constructor(posts) {
     this.posts = posts;
   }
 
-  AtomBuilder.prototype.build = function() {
+  build() {
     var atom, entries, ref, ref1;
-    entries = this.posts.sort(function(a, b) {
+    entries = this.posts.sort(function (a, b) {
       var ad, bd;
       ad = moment(a.pubdate);
       bd = moment(b.pubdate);
@@ -20,9 +18,9 @@ AtomBuilder = (function () {
       } else {
         return -1;
       }
-    }).filter(function(_, index) {
+    }).filter(function (_, index) {
       return index < 20;
-    }).map(function(post) {
+    }).map(function (post) {
       var url;
       url = 'http://blog.bouzuya.net/' + post.date.replace(/-/g, '/') + '/';
       return {
@@ -44,9 +42,4 @@ AtomBuilder = (function () {
       entries: entries
     };
   };
-
-  return AtomBuilder;
-
-})();
-
-export { AtomBuilder };
+}

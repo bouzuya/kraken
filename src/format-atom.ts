472a2d2e82dfd4c24b7364ff36e7b4af92c6f215
bouzuya <m@bouzuya.net>
2016-08-15 13:00:39 +0900

    refactor format-atom

--- src/format-atom.ts
+++ src/format-atom.ts
@@ -1,6 +1,4 @@
-import * as moment from 'moment';
-
-export type Post = {
+export type Entry = {
   date: string;
   html: string;
   pubdate: string;
@@ -27,69 +25,84 @@ type Atom = {
 };
 
 class AtomBuilder {
-  constructor(private posts: Post[]) {
-    this.posts = posts;
-  }
+  constructor(private entries: Entry[]) { }
 
   build(): Atom {
-    const entries: AtomEntry[] = this.posts
-      .sort((a, b) => {
-        const ad = moment(a.pubdate);
-        const bd = moment(b.pubdate);
-        if (ad.isSame(bd)) {
-          return 0;
-        } else if (ad.isBefore(bd)) {
-          return 1;
-        } else {
-          return -1;
-        }
-      })
-      .filter((_, index) => index < 20)
-      .map((post) => {
-        const url = `http://blog.bouzuya.net/${post.date.replace(/-/g, '/')}/`;
-        return {
-          title: post.title,
-          linkHref: url,
-          updated: post.pubdate,
-          id: url,
-          content: post.html
-        };
-      });
-    const lastEntry: AtomEntry | undefined = entries[0];
-    const updated = typeof lastEntry === 'undefined' ? '' : lastEntry.updated;
-    return {
+    const entries = this._buildAtomEntries();
+    const updated = typeof entries[0] === 'undefined' ? '' : entries[0].updated;
+    const atom = {
       title: 'blog.bouzuya.net',
       linkHref: 'http://blog.bouzuya.net/',
       updated,
       id: 'http://blog.bouzuya.net/',
-      author: {
-        name: 'bouzuya'
-      },
-      entries: entries
+      author: { name: 'bouzuya' },
+      entries
     };
+    return atom;
+  }
+
+  _buildAtomEntries(): AtomEntry[] {
+    return this.entries
+      .sort(({ pubdate: a }, { pubdate: b }) => {
+        // order by pubdate desc.
+        return a === b ? 0 : (a < b ? 1 : -1);
+      })
+      .filter((_, index) => index < 20) // limit
+      .map((entry) => {
+        const url = `http://blog.bouzuya.net/${entry.date.replace(/-/g, '/')}/`;
+        const atomEntry = {
+          title: entry.title,
+          linkHref: url,
+          updated: entry.pubdate,
+          id: url,
+          content: entry.html
+        };
+        return atomEntry;
+      });
   }
 }
 
 class AtomFormatter {
-  constructor(private atom: Atom) {
-    this.atom = atom;
-  }
+  constructor(private atom: Atom) { }
 
   format(): string {
     const atom = this.atom;
-    return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<feed xmlns=\"http://www.w3.org/2005/Atom\">\n  <title>" + (this._escapeHtml(atom.title)) + "</title>\n  <link rel=\"alternate\" type=\"text/html\" href=\"" + atom.linkHref + "\" />\n  <updated>" + atom.updated + "</updated>\n  <id>" + atom.id + "</id>\n  <author>\n    <name>" + atom.author.name + "</name>\n  </author>\n  " + (atom.entries.map(this._buildEntry.bind(this)).join('\n')) + "\n</feed>";
+    return [
+      '<?xml version= "1.0" encoding= "utf-8" ?>',
+      '<feed xmlns="http://www.w3.org/2005/Atom">',
+      `<title>${this._escapeHtml(atom.title)}</title>`,
+      `<link rel="alternate" type="text/html" href="${atom.linkHref}" />`,
+      `<updated>${atom.updated}</updated>`,
+      `<id>${atom.id}</id>`,
+      `<author><name>${atom.author.name}</name></author>`,
+      atom.entries.map(this._buildEntry.bind(this)).join('\n'),
+      '</feed>'
+    ].join('');
   }
 
   _buildEntry(entry: AtomEntry): string {
-    return "<entry>\n  <title>" + (this._escapeHtml(entry.title)) + "</title>\n  <link href=\"" + entry.linkHref + "\" />\n  <updated>" + entry.updated + "</updated>\n  <id>" + entry.id + "</id>\n  <content type=\"html\">" + (this._escapeHtml(entry.content)) + "</content>\n</entry>";
+    return [
+      '<entry>',
+      `<title>${this._escapeHtml(entry.title)}</title>`,
+      `<link href= "${entry.linkHref}" />`,
+      `<updated>${entry.updated}</updated>`,
+      `<id>${entry.id}</id>`,
+      `<content type= "html">${this._escapeHtml(entry.content)}</content>`,
+      '</entry>'
+    ].join('');
   }
 
   _escapeHtml(html: string): string {
-    return html.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+    return html
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
   }
 }
 
-const formatAtom = (entries: Post[]): string => {
+const formatAtom = (entries: Entry[]): string => {
   const atom = new AtomBuilder(entries).build();
   const formatted = new AtomFormatter(atom).format();
   return formatted;

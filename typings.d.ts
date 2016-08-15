eab2464ba1cf5cb4d5bd949a70da1bb2b4432998
bouzuya <m@bouzuya.net>
2016-08-15 11:07:09 +0900

    add typings for marked

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,10 @@
-
 declare module 'commander-b' {
   var commander: any;
   export = commander;
+}
+
+declare module 'marked' {
+  function marked(markdownString: string): string;
+  namespace marked { }
+  export = marked;
 }

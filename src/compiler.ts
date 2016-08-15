e2ae678defd1b8ee846fe5fc73932dc9eac20b6e
bouzuya <m@bouzuya.net>
2016-08-15 10:10:31 +0900

    fix variable declrations

--- src/compiler.ts
+++ src/compiler.ts
@@ -12,23 +12,58 @@ import { SitemapFormatter } from './sitemap-formatter';
 
 fs.jsonfile.spaces = null;
 
+type MyJekyll = {
+  entries: () => Entry[];
+  tagCounts: () => { [tag: string]: number };
+};
+
+type Entry = {
+  content: string;
+  file: string;
+  minutes: number;
+  pubdate: string;
+  tags: string;
+  title: string;
+};
+
+type CompiledEntry = {
+  data: string;
+  date: string;
+  minutes: number;
+  pubdate: string;
+  tags: string;
+  title: string;
+  html: string;
+};
+
 export class Compiler {
-  constructor(arg) {
-    var dstDir, postsDir;
-    postsDir = arg.postsDir, dstDir = arg.dstDir;
+  private _postsDir: string;
+  private _dstDir: string;
+  private _blog: MyJekyll;
+  private _compiledPosts: CompiledEntry[];
+
+  constructor({ dstDir, postsDir }: { dstDir: string; postsDir: string; }) {
     this._postsDir = postsDir;
     this._dstDir = dstDir;
-    this._blog = {};
+    // this._blog = {};
     this._compiledPosts = [];
   }
 
-  compile() {
+  compile(): Promise<void> {
     this._blog = myjekyll(this._postsDir + '/**/*.md', {});
-    return Promise.resolve().then(this._compilePosts.bind(this)).then(this._writeDailyPosts.bind(this)).then(this._writeMonthlyPosts.bind(this)).then(this._writeYearlyPosts.bind(this)).then(this._writeAllPosts.bind(this)).then(this._writeTagsJson.bind(this)).then(this._writeSitemapXml.bind(this)).then(this._writeAtomXml.bind(this));
+    return Promise.resolve()
+      .then(this._compilePosts.bind(this))
+      .then(this._writeDailyPosts.bind(this))
+      .then(this._writeMonthlyPosts.bind(this))
+      .then(this._writeYearlyPosts.bind(this))
+      .then(this._writeAllPosts.bind(this))
+      .then(this._writeTagsJson.bind(this))
+      .then(this._writeSitemapXml.bind(this))
+      .then(this._writeAtomXml.bind(this));
   }
 
   _compilePosts() {
-    return this._compiledPosts = this._blog.entries().map(function(entry) {
+    return this._compiledPosts = this._blog.entries().map((entry) => {
       return {
         data: entry.content,
         date: entry.file.replace(/^(\d{4}-\d{2}-\d{2})-.*$/, '$1'),
@@ -42,15 +77,14 @@ export class Compiler {
   }
 
   _writeDailyPosts() {
-    var dir, posts;
-    posts = this._compiledPosts;
-    dir = this._dstDir;
-    return async.eachSeries(posts, function(post) {
-      var d, date, dest, month, year;
-      d = moment(post.date);
-      year = d.format('YYYY');
-      month = d.format('MM');
-      date = d.format('DD');
+    const posts = this._compiledPosts;
+    const dir = this._dstDir;
+    return async.eachSeries(posts, function (post) {
+      var dest: string;
+      const d = moment(post.date);
+      const year = d.format('YYYY');
+      const month = d.format('MM');
+      const date = d.format('DD');
       dest = path.join(dir, year, month, date + '.json');
       fs.outputJsonSync(dest, post, {
         encoding: 'utf-8'
@@ -63,23 +97,23 @@ export class Compiler {
   }
 
   _writeMonthlyPosts() {
-    var dest, dir, month, monthlyPosts, posts, ref, results, year, ym;
-    monthlyPosts = this._compiledPosts.reduce(function(r, post) {
-      var d, ym;
-      d = moment(post.date);
-      ym = d.format('YYYY/MM');
-      if (r[ym] == null) {
-        r[ym] = [];
-      }
-      r[ym].push(post);
-      return r;
-    }, {});
-    dir = this._dstDir;
-    results = [];
-    for (ym in monthlyPosts) {
-      posts = monthlyPosts[ym];
-      ref = ym.split('/'), year = ref[0], month = ref[1];
-      dest = path.join(dir, year, month + '.json');
+    const monthlyPosts: { [ym: string]: CompiledEntry[]; } =
+      this._compiledPosts.reduce<{ [ym: string]: CompiledEntry[]; }>(
+        (r, post) => {
+          const d = moment(post.date);
+          const ym = d.format('YYYY/MM');
+          if (r[ym] == null) {
+            r[ym] = [];
+          }
+          r[ym].push(post);
+          return r;
+        }, {});
+    const dir = this._dstDir;
+    const results: string[] = [];
+    for (const ym in monthlyPosts) {
+      const posts = monthlyPosts[ym];
+      const [year, month] = ym.split('/');
+      let dest = path.join(dir, year, month + '.json');
       fs.outputJsonSync(dest, posts, {
         encoding: 'utf-8'
       });
@@ -92,22 +126,21 @@ export class Compiler {
   }
 
   _writeYearlyPosts() {
-    var dest, dir, posts, results, year, yearlyPosts;
-    yearlyPosts = this._compiledPosts.reduce(function(r, post) {
-      var d, y;
-      d = moment(post.date);
-      y = d.format('YYYY');
-      if (r[y] == null) {
-        r[y] = [];
-      }
-      r[y].push(post);
-      return r;
-    }, {});
-    dir = this._dstDir;
-    results = [];
-    for (year in yearlyPosts) {
-      posts = yearlyPosts[year];
-      dest = path.join(dir, year + '.json');
+    const yearlyPosts: { [year: string]: CompiledEntry[]; } = this._compiledPosts.reduce<{ [year: string]: CompiledEntry[]; }>(
+      (r, post) => {
+        const d = moment(post.date);
+        const y = d.format('YYYY');
+        if (r[y] == null) {
+          r[y] = [];
+        }
+        r[y].push(post);
+        return r;
+      }, {});
+    const dir = this._dstDir;
+    const results: string[] = [];
+    for (const year in yearlyPosts) {
+      const posts = yearlyPosts[year];
+      let dest = path.join(dir, year + '.json');
       fs.outputJsonSync(dest, posts, {
         encoding: 'utf-8'
       });
@@ -120,10 +153,9 @@ export class Compiler {
   }
 
   _writeAllPosts() {
-    var data, dest, posts;
-    posts = this._compiledPosts;
-    dest = path.join(this._dstDir, 'posts.json');
-    data = posts.map(function(i) {
+    const posts = this._compiledPosts;
+    const dest = path.join(this._dstDir, 'posts.json');
+    const data = posts.map(function (i) {
       if (i.minutes == null) {
         throw new Error(i.date + " minutes is not defined.");
       }
@@ -141,38 +173,36 @@ export class Compiler {
   }
 
   _writeTagsJson() {
-    var data, dest, tagCounts;
-    dest = path.join(this._dstDir, 'tags.json');
-    tagCounts = this._blog.tagCounts();
-    data = Object.keys(tagCounts).reduce(function(tags, tag) {
-      tags.push({
-        name: tag,
-        count: tagCounts[tag]
-      });
-      return tags;
-    }, []);
+    const dest = path.join(this._dstDir, 'tags.json');
+    const tagCounts = this._blog.tagCounts();
+    const data = Object.keys(tagCounts)
+      .reduce<{ name: string; count: number; }[]>(function (tags, tag) {
+        tags.push({
+          name: tag,
+          count: tagCounts[tag]
+        });
+        return tags;
+      }, []);
     return fs.outputJsonSync(dest, data, {
       encoding: 'utf-8'
     });
   }
 
   _writeSitemapXml() {
-    var data, dest, formatter, sitemap;
-    dest = path.join(this._dstDir, 'sitemap.xml');
-    sitemap = new SitemapBuilder(this._compiledPosts).build();
-    formatter = new SitemapFormatter(sitemap);
-    data = formatter.format();
+    const dest = path.join(this._dstDir, 'sitemap.xml');
+    const sitemap = new SitemapBuilder(this._compiledPosts).build();
+    const formatter = new SitemapFormatter(sitemap);
+    const data = formatter.format();
     return fs.outputFileSync(dest, data, {
       encoding: 'utf-8'
     });
   }
 
   _writeAtomXml() {
-    var atom, data, dest, formatter;
-    dest = path.join(this._dstDir, 'atom.xml');
-    atom = new AtomBuilder(this._compiledPosts).build();
-    formatter = new AtomFormatter(atom);
-    data = formatter.format();
+    const dest = path.join(this._dstDir, 'atom.xml');
+    const atom = new AtomBuilder(this._compiledPosts).build();
+    const formatter = new AtomFormatter(atom);
+    const data = formatter.format();
     return fs.outputFileSync(dest, data, {
       encoding: 'utf-8'
     });

75b9831a1f13db803e02de6897902cd2d86cec6e
bouzuya <m@bouzuya.net>
2016-08-15 09:12:46 +0900

    use class in cli

--- src/cli.ts
+++ src/cli.ts
@@ -2,21 +2,12 @@ import * as fs from 'fs';
 import * as commander from 'commander-b';
 import kraken from './';
 
-var CLI;
-
-CLI = (function () {
-  function CLI() {}
-
-  CLI.prototype.run = function() {
+export class CLI {
+  run() {
     var pkg;
     pkg = fs.readFileSync(__dirname + '/../package.json');
-    return commander('kraken').version(JSON.parse(pkg).version).action(function() {
+    return commander('kraken').version(JSON.parse(pkg).version).action(function () {
       return kraken();
     }).execute();
   };
-
-  return CLI;
-
-})();
-
-export { CLI };
+}

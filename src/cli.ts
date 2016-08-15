164b49bba9eda7458436a752f21cf724e6f46a02
bouzuya <m@bouzuya.net>
2016-08-15 11:04:28 +0900

    @types/node@6.0.31

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,8 @@ import kraken from './';
 
 export class CLI {
   run(): Promise<void> {
-    const pkg = fs.readFileSync(__dirname + '/../package.json');
+    const pkg = fs.readFileSync(
+      __dirname + '/../package.json', { encoding: 'utf-8' });
     return commander('kraken')
       .version(JSON.parse(pkg).version)
       .action(() => kraken())

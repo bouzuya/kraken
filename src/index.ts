3c1fe18c554dfd60f8be38bf65b0f14e382efabb
bouzuya <m@bouzuya.net>
2016-08-15 10:13:32 +0900

    refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Kraken } from './kraken';
 
 export default function () {
-  return new Kraken().run()["catch"](function(e) {
-    return console.error(e);
+  return new Kraken().run().catch((error) => {
+    return console.error(error);
   });
-};
+}

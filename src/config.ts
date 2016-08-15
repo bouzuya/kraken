1d45c6975f0c9c460122be5ed1af1499fd8dee18
bouzuya <m@bouzuya.net>
2016-08-15 10:12:31 +0900

    fix variable declarations in config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 import * as path from 'path';
 
 export class Config {
+  private _dstDir: string;
+  private _rootDir: string;
+  private _postsDir: string;
+
   load() {
     this._rootDir = process.cwd();
     this._postsDir = path.join(this._rootDir, 'data');

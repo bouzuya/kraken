4013096ad93dee21c56d2b504c4197678788c659
bouzuya <m@bouzuya.net>
2016-10-08 12:42:59 +0900

    add load

---
+++ src/load.ts
@@ -0,0 +1,8 @@
+import { Entry } from './types';
+import { Repository } from './repository';
+
+const load = (inDir: string): Entry[] => {
+  return new Repository(inDir).findAll();
+};
+
+export { load };

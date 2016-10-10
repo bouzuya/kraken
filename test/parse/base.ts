f3acada7702075715759ce132d1a7ef4cc1ae41f
bouzuya <m@bouzuya.net>
2016-10-10 09:10:54 +0900

    add test/parse/base and fix test

---
+++ test/parse/base.ts
@@ -0,0 +1,60 @@
+import beater from 'beater';
+import * as assert from 'power-assert';
+import * as sinon from 'sinon';
+import * as proxyquire from 'proxyquire';
+
+import {
+  listEntryIds as listEntryIdsType,
+  parseEntry
+} from '../../src/parse/base';
+
+const { test } = beater();
+
+const category = 'parse > base > ';
+
+test(category + 'listEntryIds', () => {
+  const listFiles = sinon.stub();
+  listFiles.returns([
+    '/home/bouzuya/data/2006-01-02.md',
+    '/home/bouzuya/data/2006-01-03-title.md',
+    '/home/bouzuya/data/2006-01-04.json'
+  ]);
+
+  const listEntryIds: typeof listEntryIdsType = proxyquire(
+    '../../src/parse/base',
+    { '../utils/fs': { listFiles } }
+  ).listEntryIds;
+
+  assert.deepEqual(listEntryIds('./data'), [
+    { year: '2006', month: '01', date: '02', title: undefined },
+    { year: '2006', month: '01', date: '03', title: 'title' }
+  ]);
+  assert(listFiles.callCount === 1);
+  assert(listFiles.getCall(0).args[0] === './data');
+});
+
+test(category + 'parseEntry', () => {
+  const meta = {
+    minutes: 10,
+    pubdate: '2006-01-02T15:04:05-07:00',
+    title: 'title'
+  };
+  const data = 'markdown';
+
+  const parse = sinon.stub();
+  parse.returns({ meta, data });
+
+  const entryId = { year: '2006', month: '01', date: '02', title: undefined };
+  assert.deepEqual(parseEntry('data', entryId, parse), {
+    id: entryId,
+    minutes: 10,
+    pubdate: '2006-01-02T15:04:05-07:00',
+    tags: [],
+    title: 'title',
+    data,
+    date: '2006-01-03', // in time zone +09:00
+    html: '<p>markdown</p>\n'
+  });
+  assert(parse.callCount === 1);
+  assert.deepEqual(parse.getCall(0).args, ['data', entryId]);
+});

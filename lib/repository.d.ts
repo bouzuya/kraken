58fd3d5f3cc8b7b8533e66cdbba140435fd6ee05
bouzuya <m@bouzuya.net>
2016-11-08 14:21:55 +0900

    3.2.1

--- lib/repository.d.ts
+++ lib/repository.d.ts
@@ -1,7 +1,10 @@
 import { Entry, EntryId } from './types';
 export declare class Repository {
-    private _entries;
+    private _dir;
+    private _ids;
+    private _parse;
     constructor(dir: string, listEntryIds: (dirOrFile: string) => EntryId[], parse: (entryDir: string, entryId: EntryId) => Entry);
+    each(f: (item: Entry) => any): void;
     findAll(): Entry[];
     findBy(query: {
         year?: string;
